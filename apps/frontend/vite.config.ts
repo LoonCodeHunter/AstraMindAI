d924ef3c709916b1948caf84a89e34848d781985
LoonBushHunt <jesseagyemang11@gmail.com>
2026-08-20 12:47:29 +0100

    Update vite.config.ts

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -4,6 +4,19 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000
+    port: 3000,
+    host: '0.0.0.0'
+  },
+  build: {
+    sourcemap: true
+  },
+  resolve: {
+    alias: {
+      '@components': '/src/components',
+      '@hooks': '/src/hooks',
+      '@services': '/src/services',
+      '@context': '/src/context',
+      '@utils': '/src/utils'
+    }
   }
 });
